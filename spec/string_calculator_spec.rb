6c15873d059cf73d0629a4255dc6534e943f2ffb
ror.shaan <ror.shaan@gmail.com>
2024-07-05 12:42:54 +0530

    add multi number sum example

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -26,6 +26,10 @@ RSpec.describe StringCalculator do
       it 'returns the number itself for a single number' do
         expect(klass.add("1")).to eq(1)
       end
+
+      it 'returns the sum of numbers for comma-separated numbers' do
+        expect(klass.add("1,5")).to eq(6)
+      end
     end
   end
 end
