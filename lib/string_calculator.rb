87c78b6d2d9329f64cb66ebe9d2b54276d677861
ror.shaan <ror.shaan@gmail.com>
2024-07-05 12:31:43 +0530

    cover invalid string scenerio

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,11 @@ class StringCalculator
   class << self
     def add(string_number = '')
       return 0 if string_number.empty?
+
+      numbers = string_number.split(',')
+      valid_numbers = numbers.select { |num| num.eql?("\n") }
+
+      return 'Invalid Input' unless valid_numbers.empty?
     end
   end
 end
