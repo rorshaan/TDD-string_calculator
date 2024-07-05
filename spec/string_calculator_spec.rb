ce6a263f08968f65c443f466623981843cc764cb
ror.shaan <ror.shaan@gmail.com>
2024-07-05 12:51:34 +0530

    write spec for custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -34,6 +34,10 @@ RSpec.describe StringCalculator do
       it 'handles new lines between numbers' do
         expect(klass.add("1\n2,3")).to eq(6)
       end
+
+      it 'supports custom delimiters' do
+        expect(klass.add("//;\n1;2")).to eq(3)
+      end
     end
   end
 end
