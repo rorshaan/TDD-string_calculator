79a900b40b9fe3e1a89d7562f07e8c8a430976a4
ror.shaan <ror.shaan@gmail.com>
2024-07-05 12:20:12 +0530

    write example for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,15 @@ require_relative './../lib/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
+    let(:klass) { StringCalculator }
+
     context 'with invalid input' do
       it 'return 0 with no parameters' do
-        expect(StringCalculator.add).to eq(0)
+        expect(klass.add).to eq(0)
+      end
+
+      it 'returns 0 for an empty string' do
+        expect(klass.add("")).to eq(0)
       end
     end
   end
